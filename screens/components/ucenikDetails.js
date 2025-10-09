775e793445f4a250b46e9e285efeabe629270ebf
Luncek-33 <l.vojnovic09@gmail.com>
2025-10-09 21:17:12 +0200

    medium module 8

--- screens/components/ucenikDetails.js
+++ screens/components/ucenikDetails.js
@@ -1,49 +1,47 @@
 import React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 const UcenikDetails = (props) => {
-    
-    return(
-        <View>
-        <View style={style.cardWrapper}>
-   <View>
-        <Image source={props.image} style={style.img}></Image>
-    </View>
+  return (
     <View>
-        <Text style={style.infoWrapper}>{props.name}</Text>
-        <Text style={style.descriptionWrapper}>{props.description}</Text>
+      <TouchableOpacity onPress={() => console.log(props.name)}>
+        <View style={style.cardWrapper}>
+          <View>
+            <Image source={props.image} style={style.img}></Image>
+          </View>
+          <View>
+            <Text style={style.infoWrapper}>{props.name}</Text>
+            <Text style={style.descriptionWrapper}>{props.description}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
     </View>
-    </View>
-    </View>
-    );
+  );
 };
 
 const style = StyleSheet.create({
-   
-    cardWrapper :{
-        flexDirection:"row",
-        backgroundColor:"white",
-        borderRadius:8,
-        width:"90%",
-        alignSelf:"center",
-        marginBottom:15,
-    },
-    img:{
-        width:100,
-        height:100,
-        borderRadius:8,
-        borderBottomLeftRadius:8,
-    },
-    infoWrapper:{
-        marginLeft:20,
-        marginTop:20,
-        fontWeight:"bold"
-    },
-    descriptionWrapper:{
-      marginLeft:20,
-       
-    },
-
+  cardWrapper: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    borderRadius: 8,
+    width: "90%",
+    alignSelf: "center",
+    marginBottom: 15,
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+    borderBottomLeftRadius: 8,
+  },
+  infoWrapper: {
+    marginLeft: 20,
+    marginTop: 20,
+    fontWeight: "bold",
+  },
+  descriptionWrapper: {
+    marginLeft: 20,
+  },
 });
 
 export default UcenikDetails;
