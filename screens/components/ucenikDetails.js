886ffbde6844f71c7471ad47cc8abbedd2788e3e
Luncek-33 <l.vojnovic09@gmail.com>
2025-10-30 10:58:41 +0100

    l 8 hard

--- screens/components/ucenikDetails.js
+++ screens/components/ucenikDetails.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons"; // koristi expo icons
 
 const UcenikDetails = (props) => {
   return (
-    <View>
-      <TouchableOpacity onPress={() => console.log(props.name)}>
-        <View style={style.cardWrapper}>
-          <View>
-            <Image source={props.image} style={style.img}></Image>
-          </View>
-          <View>
-            <Text style={style.infoWrapper}>{props.name}</Text>
-            <Text style={style.descriptionWrapper}>{props.description}</Text>
-          </View>
-        </View>
+    <View style={style.cardWrapper}>
+      <Image source={props.image} style={style.img} />
+
+      <View style={style.textWrapper}>
+        <Text style={style.infoWrapper}>{props.name}</Text>
+        <Text style={style.descriptionWrapper}>{props.description}</Text>
+      </View>
+
+      {/* Srce toggle */}
+      <TouchableOpacity onPress={props.onToggleFavorite}>
+        <AntDesign
+          name="heart"
+          size={26}
+          color={props.isFavorite ? "red" : "gray"}
+          style={style.heart}
+        />
       </TouchableOpacity>
     </View>
   );
@@ -27,20 +33,28 @@ const style = StyleSheet.create({
     width: "90%",
     alignSelf: "center",
     marginBottom: 15,
+    alignItems: "center",
+    padding: 10,
   },
   img: {
-    width: 100,
-    height: 100,
+    width: 80,
+    height: 80,
     borderRadius: 8,
-    borderBottomLeftRadius: 8,
+  },
+  textWrapper: {
+    flex: 1,
+    marginLeft: 15,
   },
   infoWrapper: {
-    marginLeft: 20,
-    marginTop: 20,
     fontWeight: "bold",
+    fontSize: 16,
   },
   descriptionWrapper: {
-    marginLeft: 20,
+    marginTop: 4,
+    fontSize: 14,
+  },
+  heart: {
+    marginRight: 10,
   },
 });
 
--- screens/ucenikScreen.js
+++ screens/ucenikScreen.js
@@ -1,25 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import UcenikDetails from "./components/ucenikDetails";
 
 const UcenikScreen = () => {
-    return(
+ 
+  const [favorites, setFavorites] = useState({
+    Luna: false,
+    Marko: false,
+    Haris: false,
+    Leon: false,
+  });
+
+ 
+  const toggleFavorite = (name) => {
+    setFavorites({
+      ...favorites,
+      [name]: !favorites[name],
+    });
+  };
+
+  return (
     <View>
-        <Text style={style.text}>Student Screen</Text>
-        <UcenikDetails name="Luna" image={require("../assets/avatar2.png")} description="Prvi student" />
-        <UcenikDetails name="Marko" image={require("../assets/avatar3.png")} description="Drugi student"/>
-        <UcenikDetails name="Haris" image={require("../assets/avatar6.png")} description="Treći student"/>
-        <UcenikDetails name="Leon" image={require("../assets/avatar5.jpg")} description="Četvrti student"/>
+      <Text style={style.text}>Student Screen</Text>
+
+      <UcenikDetails
+        name="Luna"
+        image={require("../assets/avatar2.png")}
+        description="Prvi student"
+        isFavorite={favorites.Luna}
+        onToggleFavorite={() => toggleFavorite("Luna")}
+      />
+
+      <UcenikDetails
+        name="Marko"
+        image={require("../assets/avatar3.png")}
+        description="Drugi student"
+        isFavorite={favorites.Marko}
+        onToggleFavorite={() => toggleFavorite("Marko")}
+      />
+
+      <UcenikDetails
+        name="Haris"
+        image={require("../assets/avatar6.png")}
+        description="Treći student"
+        isFavorite={favorites.Haris}
+        onToggleFavorite={() => toggleFavorite("Haris")}
+      />
+
+      <UcenikDetails
+        name="Leon"
+        image={require("../assets/avatar5.jpg")}
+        description="Četvrti student"
+        isFavorite={favorites.Leon}
+        onToggleFavorite={() => toggleFavorite("Leon")}
+      />
     </View>
-    );
+  );
 };
 
 const style = StyleSheet.create({
-    text:{
-        textAlign: "center",
-        fontSize: 20,
-        marginVertical:20,
-    },
+  text: {
+    textAlign: "center",
+    fontSize: 20,
+    marginVertical: 20,
+  },
 });
 
 export default UcenikScreen;
